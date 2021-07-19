5577345d94928075d4f1bcf764d9d24a74af5647
Potato Man <notserr1998@gmail.com>
2021-07-19 12:28:20 +0530

    Now the user can choose the mixer they want to use

--- src/com/webion/voicemod/Main.java
+++ src/com/webion/voicemod/Main.java
@@ -1,6 +1,22 @@
-package com.webion.voicemod;    
+package com.webion.voicemod;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        new AudioPlayer(6);
+        Scanner sc = new Scanner(System.in);
+        new AudioPlayer(getMixerIndex(sc));
+        sc.close();
+    }
+    public static int getMixerIndex(Scanner sc){
+        Mixer.Info[]mixerInfo = AudioSystem.getMixerInfo();
+        for(int i  = 0; i < mixerInfo.length; i ++){
+            System.out.print("[" + i + "]");
+            System.out.println(mixerInfo[i].toString());
+        }
+        System.out.print("What is the index of your desired Mixer ? : ");
+        return sc.nextInt();
     }
 }
